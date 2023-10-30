05b704260e5a862e1e9c68dd6a5c43d168716388
Jin Kun Chai <chaijinkun@gmail.com>
2023-10-30 09:19:01 -0500

    Create app12.py

---
+++ app12.py
@@ -0,0 +1,10 @@
+import streamlit as st 
+from streamlit_chat import message
+import tempfile
+from langchain.document_loaders.csv_loader import CSVLoader
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.llms import CTransformers
+from langchain.chains import ConversationalRetrievalChain
+
+
